2c481ee6faffa68f18a84e633b24f29c58d79d46
Kevin Benton <kbenton@activecampaign.com>
2019-07-22 16:04:17 -0500

    Exercise 10-1

--- ch10/ex10-1.py
+++ ch10/ex10-1.py
@@ -7,3 +7,20 @@
 # object, and once by storing the lines in a list then working with them
 # outside the with block.
 
+filename = 'learning_python.txt'
+
+with open(filename) as file_object:
+    contents = file_object.read()
+    print(contents.rstrip().replace('Python', 'C#'))
+
+print('')
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip().replace('Python', 'C#'))
+
+print('')
+with open(filename) as file_object:
+    lines = file_object.readlines()
+for line in lines:
+    print(line.rstrip().replace('Python', 'C#'))
+
