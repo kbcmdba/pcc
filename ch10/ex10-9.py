dca5795c1d837049e4504f26351916057daffffe
Kevin Benton <kbenton@activecampaign.com>
2019-07-23 10:21:19 -0500

    Minor change.

--- ch10/ex10-9.py
+++ ch10/ex10-9.py
@@ -1,3 +1,12 @@
 # Silent Cats and Dogs: Modify your except block in Exercise 10-8 to fail
 # silently if either file is missing.
 
+
+filenames = ['cats.txt', 'dogs.txt']
+for filename in filenames:
+    try:
+        with open(filename, 'r') as file_object:
+            print(file_object.read())
+    except FileNotFoundError:
+        pass
+
