4398a5ce13f0b20f3002e0b6c91b01f1d3ad3f69
Kevin Benton <kbenton@activecampaign.com>
2019-07-23 21:42:10 -0500

    Exercises for Ch11

---
+++ ch11/ex11-2.py
@@ -0,0 +1,13 @@
+# Population: Modify your function so it requires a third parameter,
+# population. It should now return a signle string of the form City,
+# Country - population xxx, such as Santiago, Chile - population 5000000.
+# Run test_cities.py again. Make sure test_city_country() fails this time.
+#
+# Modify the function so the populatin parameter is optional. Run
+# test_cities.py again, and make sure test_city_country() passes again.
+#
+# Write a second test called test_city_country_population() that verifies you
+# can call your function with the values 'santiago', 'chillie', and
+# 'population=50000000'. Run test_cities again, and make sure this new test
+# passes.
+
