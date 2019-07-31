bd59a05f2bacc916efbda992a5458df759abadb2
Kevin Benton <kbenton@activecampaign.com>
2019-07-31 15:12:30 -0500

    Completed up to just before ex13-1.py

--- ch13/alien_invasion.py
+++ ch13/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -13,10 +14,16 @@ def run_game():
     screen = pygame.display.set_mode(display_mode)
     pygame.display.set_caption("Alien Invasion")
 
-    # Make a ship.
+    # Make a ship, a group of bullets, and a group of aliens.
     ship = Ship(ai_settings, screen)
-    # Make a group to store bullets in.
     bullets = Group()
+    aliens = Group()
+
+    # Create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    #    # Make an alien.
+    #    alien = Alien(ai_settings, screen)
 
     # Start the main loop for the game.
     while True:
@@ -24,7 +31,7 @@ def run_game():
         ship.update()
         bullets.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
 
--- ch13/ship.py
+++ ch13/ship.py
@@ -12,9 +12,9 @@ class Ship():
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
-        # Start each new ship at the  center of the screen.
+        # Start each new ship at the bottom center of the screen.
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.centery = self.screen_rect.centery
+        self.rect.bottom = self.screen_rect.bottom
 
         # Store decimal values for the ship's center.
         self.centerx = float(self.rect.centerx)
