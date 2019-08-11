5e7abcff6783714f77c17d3ba85661d97500c195
Kevin Benton <kbenton@activecampaign.com>
2019-08-11 15:33:47 -0500

    Added exercises 13-3 and 13-4

---
+++ ch13/ex13-4.py
@@ -0,0 +1,4 @@
+# Steady Rain: Modify your code in Exericse 13-3 so that when a row of
+# raindrops disappears off the bottom of the screen, a new row appears at the
+# top of the screen and begins to fall.
+
