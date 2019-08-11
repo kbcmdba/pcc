f5b7211f1c63b9469566808aa89ae47e4fcf7335
Kevin Benton <kbenton@activecampaign.com>
2019-08-11 15:29:00 -0500

    Aliens menace toward the bottom of the screen.

--- ch13/alien_invasion.py
+++ ch13/alien_invasion.py
@@ -30,8 +30,8 @@ def run_game():
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        bullets.update()
         gf.update_bullets(bullets)
+        gf.update_aliens(ai_settings, aliens)
         gf.update_screen(ai_settings, screen, stars, ship, aliens, bullets)
 
 run_game()
--- ch13/settings.py
+++ ch13/settings.py
@@ -19,3 +19,9 @@ class Settings():
         # Star settings
         self.stars_allowed = 50
 
+        # Alien settings
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
