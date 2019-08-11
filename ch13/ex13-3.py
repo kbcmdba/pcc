505a5b843a4c2cf875ac75117bdc62e4dc62861e
Kevin Benton <kbenton@activecampaign.com>
2019-08-11 16:26:27 -0500

    Skipped exercises 13-3 and 13-4

--- ch13/ex13-3.py
+++ ch13/ex13-3.py
@@ -1,2 +1,4 @@
 # Raindrops: Find an image of a raindrop and create a grid of raindrops. Make
 # the raindrops fall toward the bottom of the screen until they disappear.
+
+# Skipped
--- ch13/ex13-4.py
+++ ch13/ex13-4.py
@@ -2,3 +2,4 @@
 # raindrops disappears off the bottom of the screen, a new row appears at the
 # top of the screen and begins to fall.
 
+# Skipped
