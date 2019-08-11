4008703ada445fc9c8944ce83740fc65573bdbce
Kevin Benton <kbenton@activecampaign.com>
2019-08-11 17:05:37 -0500

    Skipping exercise 13-5.

---
+++ ch13/ex13-5.py
@@ -0,0 +1,9 @@
+# Catch: Create a game tha tplaces a character that you can move left and
+# right at the bottom of the screen. Make a ball appear at a random position
+# at the top of the screen and fall down the screen at a steady rate. If your
+# character "catches" the ball by colliding with it, make the ball disappear.
+# Make a new ball each time your character catches the ball or whenever the
+# ball disappears off the bottom of the screen.
+#
+# Skipped.
+
